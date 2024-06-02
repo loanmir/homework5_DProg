a5cbbed86a1077dae6cd5e9a6aa4a6ffa1a30092
loanmir <lucas.jakin02@gmail.com>
2024-06-02 16:33:25 +0200

    Homework 5 finished!

--- hw5.R
+++ hw5.R
@@ -2,7 +2,6 @@ library(readr)
 library(tidyverse)
 library(ggplot2)
 dataset <- read.csv("dataset.csv", sep = ";")
-View(dataset)
 state_abrv_pop <- read.csv("state_abbreviations_and_populations.csv", sep = ";")
 
 state_abrv_pop
@@ -13,6 +12,7 @@ data_merged <- inner_join(state_abrv_pop, dataset, by = c("Abbreviation" = "Stat
 
 fatalities_state <- data_merged %>% group_by(State = .$Name.x) %>%
   summarise(n = n())
+fatalities_state
 #b)
 ggplot(fatalities_state, aes(x = n, y = State)) +
   geom_histogram(stat = "identity") +
@@ -21,5 +21,22 @@ ggplot(fatalities_state, aes(x = n, y = State)) +
        y = "State")
 
 
-# exercise number2
+# exercise number 2
+gender_raceData <- dataset %>% group_by(Gender, Race) %>% summarise(n = n(), .groups = 'drop')
+ggplot(gender_raceData, aes(x = n, y = Race))+
+  geom_col() +
+  facet_wrap(~Gender, scales = "free_x")+
+  labs(x = "count", y = "Race") 
+ 
 
+
+# exercise number 3
+# Plot the outline of the USA by scatterplotting the locations of deaths
+dataset <- dataset %>% mutate(LongitudeNew = as.numeric(Longitude),
+                              LatitudeNew = as.numeric(Latitude))
+dataset_clean <- dataset %>% filter(!is.na(LongitudeNew) & !is.na(LatitudeNew))
+ggplot(dataset_clean, aes(x = LongitudeNew, y = LatitudeNew)) +
+  geom_point(color = "black")+
+  labs(x = "Longitude",
+       y = "Latitude")+
+  theme_minimal()
